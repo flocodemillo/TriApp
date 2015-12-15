4e3c02ec76f73fb573f747adca895e647789b433
floco <vasily.rodionovich@gmail.com>
2015-12-15 13:04:15 +0100

    Patient QR

--- app/src/main/java/es/usc/citius/triapp/data/Patient.java
+++ app/src/main/java/es/usc/citius/triapp/data/Patient.java
@@ -1,26 +1,27 @@
 package es.usc.citius.triapp.data;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Patient {
 
     private String name;
-    private String lastName;
+    private String mail;
+    private String telephone;
     private String id;
-    private String birthDate;
-    private List<TriageResult> result = new ArrayList<>();
 
-    public Patient(String name, String lastName, String date, String id) {
+    //private String birthDate; 
+    //private List<TriageResult> result = new ArrayList<>();
+
+    public Patient(String name, String telephone, String mail, String id) {
         this.name = name;
-        this.lastName = lastName;
-        this.birthDate = date;
+        this.telephone = telephone;
+        this.mail = mail;
         this.id = id;
     }
 
-    public String getName() { return this.name; }
-    public String getLastName() { return this.lastName; }
-    public String getID() { return this.id; }
-    public String getBirthDate() { return this.birthDate; }
+    public String getName() {return this.name;}
+    public String getMail() {return this.mail;}
+    public String getID() {return this.id;}
+    public String getTelephone() {return this.telephone;}
 }
